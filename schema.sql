2a9fe6b56ee0c122e2cf9052ec46b7035bf159ff
manthan <mehta.manth@northeastern.edu>
2021-11-23 15:49:00 -0500

    added the erdplus file

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS customer;
+
+CREATE TABLE customer (
+  customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  email varchar(255) NOT NULL,
+  phone_number NUMERIC NOT NULL,
+  rating FLOAT,
+  wallet_balance float not null,
+  address varchar not null
+);
+
